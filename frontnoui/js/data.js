5342340c1deef98a16383e5182bc5a05001dd3ce
tthing55 <ymrgm2005@gmail.com>
2025-11-28 04:06:26 +0900

    data 관련 js

---
+++ frontnoui/js/data.js
@@ -0,0 +1,59 @@
+window.mockPlants = [
+  {
+    id: '1',
+    name: '몬스테라',
+    species: 'Monstera Deliciosa',
+    nickname: '초록이',
+    adoptionDate: '2024-06-28',
+    imageUrl: 'https://images.unsplash.com/photo-1648528203163-8604bf696e7c',
+    lastWatered: '2025-10-20',
+  },
+  {
+    id: '2',
+    name: '산세비에리아',
+    species: 'Sansevieria Trifasciata',
+    nickname: '공기요정',
+    adoptionDate: '2024-08-15',
+    imageUrl: 'https://images.unsplash.com/photo-1613498630970-f2a333cb4974',
+    lastWatered: '2025-10-18',
+  },
+  {
+    id: '3',
+    name: '스투키',
+    species: 'Sansevieria Stuckyi',
+    nickname: '뾰족이',
+    adoptionDate: '2024-09-01',
+    imageUrl: 'https://images.unsplash.com/photo-1550207477-85f418dc3448',
+    lastWatered: '2025-10-22',
+  },
+];
+
+window.mockJournals = [
+  {
+    id: '1',
+    plantId: '1',
+    plantNickname: '초록이',
+    date: '2025-10-24',
+    images: ['https://images.unsplash.com/photo-1624421719748-179e1a8e956d'],
+    memo: '새순이 나왔어요! 정말 기쁘네요 ☺️',
+    tags: ['새순', '성장'],
+  },
+  {
+    id: '2',
+    plantId: '1',
+    plantNickname: '초록이',
+    date: '2025-10-20',
+    images: ['https://images.unsplash.com/photo-1648528203163-8604bf696e7c'],
+    memo: '오늘 물을 주었습니다. 흙이 많이 말라있었어요.',
+    tags: ['물주기'],
+  },
+  {
+    id: '3',
+    plantId: '2',
+    plantNickname: '공기요정',
+    date: '2025-10-18',
+    images: ['https://images.unsplash.com/photo-1613498630970-f2a333cb4974'],
+    memo: '잎이 더욱 건강해 보여요.',
+    tags: ['성장'],
+  },
+];
